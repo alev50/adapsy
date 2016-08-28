c1d8744d32363a893ff952cbc89731b0295f48de
Anthony Levesque <alev50@gmail.com>
2016-08-28 22:54:29 +0200

    First connection to PostgreSQL database using JDBC

--- adapsy-webapp/src/main/java/com/adapsy/webapp/controller/SampleController.java
+++ adapsy-webapp/src/main/java/com/adapsy/webapp/controller/SampleController.java
@@ -1,18 +1,60 @@
 package com.adapsy.webapp.controller;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.text.MessageFormat;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
+	
+	@Autowired
+	DataSource datasource;
 
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello World!";
+    	String output = null;
+    	
+    	try {
+	    	Connection conn = datasource.getConnection();
+	        
+	    	try {
+	    		Statement stat = conn.createStatement();
+	    		
+	    		try {
+	    			ResultSet res = stat.executeQuery("select count(*) from contact");
+	    			
+	    			try {
+	    				if (res.next()) {
+	    					int nb = res.getInt(1);
+	    					
+	    					output = MessageFormat.format("Il y a {0} contact(s) dans la base.", nb);
+	    				}
+	    			} finally {
+	    				res.close();
+	    			}
+	    		} finally {
+	    			stat.close();
+	    		}
+	    	} finally {
+	    		conn.close();
+	    	}
+    	} catch (Exception e) {
+    		output = e.getMessage();
+    	}
+    	
+    	return output;
     }
 
     public static void main(String[] args) throws Exception {
